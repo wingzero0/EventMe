a2536459820537d2acfb92d983cae79deb8b666c
wingzero <wingzzz2003@hotmail.com>
2013-07-13 14:44:33 +0800

    ActivitySuggestion/userProfile: create a php class for UserProfile. Move code from profileHandler.php to  class UserProfile
    ActiivitySuggestion/profileHandler.php, add get operation

--- ActivitySuggestion/profileHandler.php
+++ ActivitySuggestion/profileHandler.php
@@ -1,30 +1,49 @@
 <?php
 /*
  * handle the request of updating profile keyword:
- * GET parameter
- * 1. "op": str value should be "insert"
- * 		if value is "insert", program will insert keywords and their 
- * 		corresponding wegihting to db, all keywords will be treated as a profile.
+ * GET parameter (Input)
+ * 1. "op": str value should be "insert" or "get"
+ * 		if value is "insert", GET fields "userID", "num", "keywordIDX", "wight" will also be required
+ * 			this program will insert keywords and their corresponding wegihting to db, 
+ * 			all keywords will be treated as a profile.
+ * 		if value is "get", GET fields "userID" will be required
+ * 			the program will retrieve all profile keywordIDs of the user.
  * 2. "num": int value indicate that how many keywords will be queried
  * 		if it is bigger than 0, it means that there will be 
  * 			"keywordID0", "keywordID1" ... "keywordIDX", "weight0", "weight1" ... "weightX"
  * 			(X = num) in the Get parameter for query
  * 3. "keywordIDX": int value
  * 		the value must exist in table `Keyword` in database, or it will trigger error.
- * 4. "weightX" : double value
+ * 4. "weight" : double value
  * 		the value represents the wegihting of keywordIDX in profile.
+ *
+ * Return value it will return a json array.
+ * For "op" = "insert"  
+ *	json["ret"]: equal to 1 if successful, equal to -1 if fail.
+ *  json["error"]: if "ret" is -1, then "error" will be assigned a string for error cause.
  * 
- * return value: it will return a json array.
- * 	json index "ret": equal to 1 if successful, equal to -1 if fail.
- *  json index "error": if "ret" is -1, then "error" will be assigned a string for error cause.
- *   
+ * For "op" = "get"
+ *  json["ret"]: equal to 1 if successful, equal to -1 if fail.
+ *  json["error"]: if "ret" is -1, then "error" will be assigned a string for error cause.
+ *  json["objs"]: array of sql result
+ *  json["objs"][x]["profileID"]: int value, profile id
+ *  json["objs"][x]["keywordID"]: int value, keyword id
+ *  json["objs"][x]["weight"]: double value, the keyword weight in the corresponding profile
+ *  p.s. one profile will contain many keywords, one keyword will appear in many profile (of a user)  
+ *  
  * sample usage
- * profileHandler.php?op=insert&userID=1&num=2&keywordID0=9&keywordID1=10&weight0=1&weight1=55
+ * 
+ * insert:
+ * 	http://140.112.29.228/ActivitySuggestion/profileHandler.php?op=insert&userID=2&num=2&keywordID0=3&keywordID1=2&weight=11.11
+ * 	return json {"ret":1}
+ * get:
+ *  http://140.112.29.228/ActivitySuggestion/profileHandler.php?op=get&userID=2
+ *  return json {"ret":1,"sqlResult":[{"profileID":"5","keywordID":"2","weight":"11.11"},{"profileID":"5","keywordID":"3","weight":"11.11"}]}
  */
 
 require_once 'connection.php';
 require_once "utility.php";
-
+require_once "userProfile.php";
 
 $ret = array();
 $ret["ret"] = -1;
@@ -32,73 +51,23 @@ $ret["ret"] = -1;
 if (isset($_GET["op"]) && $_GET["op"] == "insert"){
 	$s_var = array();
 
-	Utility::AddslashesToGETField("userID", $s_var);
-	Utility::AddslashesToGETField("num", $s_var); // number of keyword in the profile
+	Utility::AddslashesToGETField("userID", $s_var, "int");
+	Utility::AddslashesToGETField("num", $s_var, "int"); // number of keyword in the profile
 	
-	$num = intval($s_var["num"]);
 	Utility::AddslashesToGETField("weight", $s_var, "double"); // weighting should be double, one weighting for each keyword
-	for ($i = 0;$i<$num ;$i++){
+	for ($i = 0;$i<$s_var["num"] ;$i++){
 		Utility::AddslashesToGETField("keywordID".$i, $s_var, "int"); // each keywordID should be int
 		// Utility::AddslashesToGETField("weight".$i, $s_var, "double"); // each weighting should be double
 	}
 	
-	$sql = 'lock tables `Keyword` read, `UserProfile` write, `ProfileKeyword` write';
-	$g_mysqli->query($sql);
-	if ($g_mysqli->error){
-		$ret["error"] = $g_mysqli->error;
-		echo json_encode($ret);
-		return;
-	}
-	
-	// check reference keyword id's existance
-	for ($i =0;$i<$num;$i++){
-		$sql = sprintf("SELECT `id` FROM  `Keyword` WHERE `id` =%d", 
-				$s_var["keywordID".$i]);
-		$result = $g_mysqli->query($sql);
-		
-		if ($g_mysqli->error){
-			$ret["error"] = $g_mysqli->error;		
-			echo json_encode($ret);
-			Utility::UnlockTables($g_mysqli);
-			return;
-		}else if (  !($result->fetch_row()) ) {
-			// no refernce keyword
-			$ret["error"] = "keyword id " . $s_var["keywordID".$i] . " doesn't exist";
-			echo json_encode($ret);
-			Utility::UnlockTables($g_mysqli);
-			return;
-		}
-	}
-	
-	// create new profile, get profile id,
-	$sql = sprintf("insert into `UserProfile` (`UserID`) value ('%s')", $s_var["userID"]);
-	$result = $g_mysqli->query($sql);
-	
-	if ($g_mysqli->error){
-		$ret["error"] = $g_mysqli->error;
-		echo json_encode($ret);
-		Utility::UnlockTables($g_mysqli);
-		return;
-	}
-	$profileID = $g_mysqli->insert_id;
-	
-	// insert profile id, keyword id to ProfileKeyword table
-	for ($i = 0;$i<$num;$i++){
-		$sql = sprintf("insert into `ProfileKeyword` (`ProfileID`, `KeywordID`, `Weight`) value (%d, %d, %lf)",
-				//$profileID, $s_var["keywordID".$i], $s_var["weight".$i]);
-				$profileID, $s_var["keywordID".$i], $s_var["weight"]);
-		$result = $g_mysqli->query($sql);
-		
-		if ($g_mysqli->error){
-			$ret["error"] = $g_mysqli->error;
-			echo json_encode($ret);
-			Utility::UnlockTables($g_mysqli);
-			return;
-		}
-	}
-	
-	$ret["ret"] = 1;
-	Utility::UnlockTables($g_mysqli);
+	$ret = UserProfile::InsertUserProfile($s_var);
+	echo json_encode($ret);
+}else if (isset($_GET["op"]) && $_GET["op"] == "get"){
+	$s_var = array();
+	Utility::AddslashesToGETField("userID", $s_var, "int");
+	$ret = UserProfile::GetUserProfileKeyword($s_var["userID"]);
+	$ret["objs"] = $ret["sqlResult"];
+	unset($ret["sqlResult"]);
 	echo json_encode($ret);
 }
 ?>
---
+++ ActivitySuggestion/userProfile.php
@@ -0,0 +1,129 @@
+<?php
+/* handle the db requst that involve with table UserProfile
+ */
+
+require_once 'connection.php';
+require_once 'utility.php';
+
+class UserProfile{
+	public function __construct(){
+		
+	}
+	public static function GetUserProfileKeyword($uid, $returnByKeywordID = true){
+		global $g_mysqli;
+		
+		if ($returnByKeywordID){
+			$sql = sprintf(
+				"SELECT U.ProfileID as profileID, PK.keywordID as keywordID, PK.weight as weight
+				FROM UserProfile U, ProfileKeyword PK
+				WHERE U.UserID = %d
+				AND U.ProfileID = PK.profileID",
+				$uid);
+		}else{
+			$sql = sprintf(
+				"SELECT U.ProfileID as profileID, K.Keyword as keyword, PK.weight as weight
+				FROM UserProfile U, ProfileKeyword PK, Keyword K
+				WHERE U.UserID = %d
+				AND U.ProfileID = PK.profileID
+				AND PK.keywordID = K.id",
+				$uid);
+		}
+		$result = $g_mysqli->query($sql);
+		
+		$ret = array();
+		$ret['ret'] = -1;
+		if ($g_mysqli->error){
+			$ret['error'] = $g_mysqli->error;
+			return $ret;
+		}
+		
+		$keywords = array();
+		if ($returnByKeywordID){
+			$i = 0;
+			while($row = $result->fetch_assoc()){
+				$keywords[$i]["profileID"] = intval($row["profileID"]);
+				$keywords[$i]["keywordID"] = intval($row["keywordID"]);
+				$keywords[$i]["weight"] = doubleval($row["weight"]);
+				$i++;
+			}
+		}else{
+			$i = 0;
+			while($row = $result->fetch_assoc()){
+				$keywords[$i]["profileID"] = intval($row["profileID"]);
+				$keywords[$i]["keyword"] = $row["keyword"];
+				$keywords[$i]["weight"] = doubleval($row["weight"]);
+				$i++;
+			}
+		}
+		$ret['ret'] = 1;
+		$ret['sqlResult'] = $keywords;
+		return $ret;
+	}
+	public static function InsertUserProfile($s_var){
+		global $g_mysqli;
+		
+		$num = $s_var["num"];
+		$sql = 'lock tables `Keyword` read, `UserProfile` write, `ProfileKeyword` write';
+		$g_mysqli->query($sql);
+		if ($g_mysqli->error){
+			$ret["error"] = $g_mysqli->error;
+			//echo json_encode($ret);
+			return $ret;
+		}
+
+		// check reference keyword id's existance
+		for ($i =0;$i<$num;$i++){
+			$sql = sprintf("SELECT `id` FROM  `Keyword` WHERE `id` =%d", 
+					$s_var["keywordID".$i]);
+			$result = $g_mysqli->query($sql);
+
+			if ($g_mysqli->error){
+				$ret["error"] = $g_mysqli->error;		
+				//echo json_encode($ret);
+				Utility::UnlockTables($g_mysqli);
+				return $ret;
+			}else if (  !($result->fetch_row()) ) {
+				// no refernce keyword
+				$ret["error"] = "keyword id " . $s_var["keywordID".$i] . " doesn't exist";
+				//echo json_encode($ret);
+				Utility::UnlockTables($g_mysqli);
+				return $ret;
+			}
+		}
+
+		// create new profile, get profile id,
+		$sql = sprintf("insert into `UserProfile` (`UserID`) value ('%s')", $s_var["userID"]);
+		$result = $g_mysqli->query($sql);
+
+		if ($g_mysqli->error){
+			$ret["error"] = $g_mysqli->error;
+			//echo json_encode($ret);
+			Utility::UnlockTables($g_mysqli);
+			return $ret;
+		}
+		$profileID = $g_mysqli->insert_id;
+
+		// insert profile id, keyword id to ProfileKeyword table
+		for ($i = 0;$i<$num;$i++){
+			$sql = sprintf("insert into `ProfileKeyword` (`ProfileID`, `KeywordID`, `Weight`) value (%d, %d, %lf)",
+					//$profileID, $s_var["keywordID".$i], $s_var["weight".$i]);
+				$profileID, $s_var["keywordID".$i], $s_var["weight"]);
+			$result = $g_mysqli->query($sql);
+
+			if ($g_mysqli->error){
+				$ret["error"] = $g_mysqli->error;
+				//echo json_encode($ret);
+				Utility::UnlockTables($g_mysqli);
+				return $ret;
+			}
+		}
+
+		$ret["ret"] = 1;
+		Utility::UnlockTables($g_mysqli);
+		//echo json_encode($ret);
+		return $ret;
+
+	}
+}
+
+?>
